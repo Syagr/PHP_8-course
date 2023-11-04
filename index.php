627f6785279380996326cb50a7b84f06e6f674ff
Syagr <114944986+Syagr@users.noreply.github.com>
2023-11-04 10:57:43 +0200

    first variables on the php

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+echo 1111111111111111;
+
+$float= 10.10;
+
+$str = 'Hello world';
+$num = 10;
+$str = "Hello world $num";
+
+echo $str;
+
+$bool = true // false
+$arr = [10, 'Hello world'];
+
+print_r($arr);
+
+$null = null;
+
+$num = 10;
+
+$res = $num + 10;
+
+echo $num;
+
+echo $null;
+var_dump($null);
